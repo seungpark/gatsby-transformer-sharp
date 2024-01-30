1084e0a094e0675a52549fc5018ecec368a6b8cc
Seung Park <seung.park@mongodb.com>
2024-01-30 09:36:02 -0500

    first commit

---
+++ src/on-node-create.js
@@ -0,0 +1,33 @@
+const { supportedExtensions } = require(`./supported-extensions`)
+
+function shouldOnCreateNode({ node }) {
+  return node.internal.type === `File` && !!supportedExtensions[node.extension]
+}
+
+module.exports.shouldOnCreateNode = shouldOnCreateNode
+
+module.exports.onCreateNode = async function onCreateNode({
+  node,
+  actions,
+  createNodeId,
+}) {
+  const { createNode, createParentChildLink } = actions
+
+  const imageNode = {
+    id: createNodeId(`${node.id} >> ImageSharp`),
+    children: [],
+    parent: node.id,
+    internal: {
+      contentDigest: `${node.internal.contentDigest}`,
+      type: `ImageSharp`,
+    },
+  }
+
+  console.log('calling createNode and createParentChildLink');
+  console.log(`imageNode: ${JSON.stringify({id: imageNode.id, internal: imageNode.internal})}`);
+  console.log(`parentNode: ${JSON.stringify({id: node.id, internal: node.internal})}`);
+  createNode(imageNode)
+  createParentChildLink({ parent: node, child: imageNode })
+
+  return
+}
